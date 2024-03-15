9743727b79f81298983bff02750f1772fb87e5ff
StivenM100 <stiventingo20@gmail.com>
2024-03-15 01:20:22 -0500

    Reto terminado

--- reto-principiantes.py
+++ reto-principiantes.py
@@ -174,7 +174,112 @@ while bandera:
                 consumo = X / Y
                 
                 print(f"{consumo:.3f} km/l")
-
+        elif opcion == 1015:
+                # Ejercicio 1015
+                punto_1 = input("Ingrese punto p1: (x1 y1) \n").split(" ")
+                punto_2 = input("Ingrese punto p2: (x2 y2) \n").split(" ")
+                
+                X1 = float(punto_1[0])
+                Y1 = float(punto_1[1])
+                X2 = float(punto_2[0])
+                Y2 = float(punto_2[1])
+                
+                distancia = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+                
+                print(f"La distancia entre los dos puntos es: {distancia:.4f}")
+        elif opcion == 1016:
+                # Ejercicio 1016
+                distancia = int(input("Ingrese la distancia que existe entre los dos autos \n"))
+                tiempo = 2 * distancia
+                print(f"El tiempo que le lleva al auto Y tomar esa distancia es: {tiempo} minutos")
+        elif opcion == 1017:
+                # Ejercicio 1017
+                tiempo = int(input("Ingrese el tiempo que duró el viaje en horas \n"))
+                velocidad = int(input("Ingrese la velocidad media del viaje en Km/h \n"))
+                distancia = tiempo * velocidad
+                litros = distancia / 12
+                print(f"La cantidad de litros de combustible que fueron necesarios para hacer el viaje es de: {litros:.3f}")
+        elif opcion == 1018:
+                # Ejercicio 1018
+                valor = int(input("Ingrese valor entero para calcular el menor número posible de billetes en que puede ser descompuesto \n"))
+                print(valor)
+                
+                for i in [100, 50, 20, 10, 5, 2, 1]:
+                        print(f"{valor // i} nota(s) de R$ {i},00")
+                        valor %= i
+        elif opcion == 1019:
+                # Ejercicio 1019
+                tiempo = int(input("Ingrese tiempo en segundos del evento realizado en la fábrica \n"))
+                div_horas = tiempo % 3600
+                div_minutos = div_horas % 60
+                print(f"El tiempo en horas:minutos:segundos es ---> {int(tiempo // 3600)}:{int(div_horas // 60)}:{int(div_minutos)}")
+        elif opcion == 1020:
+                # Ejercicio 1020
+                edad = int(input("Ingrese la edad de una persona en días \n"))
+                div_ano = edad % 365
+                dias = div_ano % 30
+                print("Su edad en años, meses y dias es:")
+                print(f"{int(edad // 365)} ano(s)")
+                print(f"{int(div_ano // 30)} mes(es)")
+                print(f"{int(dias)} dia(s)")
+        elif opcion == 1021:
+                # Ejercicio 1021
+                valor = float(input("Ingrese valor monetario hasta con dos decimales \n"))
+                
+                print('NOTAS:')
+                
+                for i in [100, 50, 20, 10, 5, 2]:
+                        print(f'{int(valor/i)} nota(s) de R$ {i}.00')
+                        valor = float(f'{valor%i:.2f}')
+                
+                print('MOEDAS:')
+                
+                for i in [1, 0.50, 0.25, 0.10, 0.05, 0.01]:
+                        print(f'{int(valor/i)} moeda(s) de R$ {i:.2f}')
+                        valor = float(f'{valor%i:.2f}')
+        elif opcion == 1035:
+                # Ejercicio 1035
+                valores = input("Ingrese cuatro valores enteros [a b c d] \n").split(" ")
+                
+                A = int(valores[0])
+                B = int(valores[1])
+                C = int(valores[2])
+                D = int(valores[3])
+                
+                if B > C and D > A and (C + D) > (A +B) and C > 0 and D > 0 and A % 2 == 0:
+                        print("Valores aceitos")
+                else:
+                        print("Valores nao aceitos")
+        elif opcion == 1036:
+                # Ejercicio 1036
+                valores = input("Ingrese tres números [a b c] \n").split(" ")
+                
+                a = float(valores[0])
+                b = float(valores[1])
+                c = float(valores[2])
+                
+                formula = b**2 - 4 * a * c
+                
+                if a == 0 or formula < 0:
+                        print('Impossivel calcular')
+                else:
+                        x1 = (- b + formula**0.50) / (2 * a)
+                        x2 = (- b - formula**0.50) / (2 * a)
+                        print(f'R1 = {x1:.5f}')
+                        print(f'R2 = {x2:.5f}')
+        elif opcion == 1037:
+                numero = float(input("Ingrese número de punto flotante para determinar a que intervalo pertenece \n"))
+                
+                if numero < 0 or numero > 100:
+                        print("Fora de intervalo")
+                if numero >= 0 and numero <= 25:
+                        print("Intervalo [0,25]")
+                if numero > 25 and numero <= 50:
+                        print("Intervalo (25,50]")
+                if numero > 50 and numero <= 75:
+                        print("Intervalo (50,75]")
+                if numero > 75 and numero <= 100:
+                        print("Intervalo (75,100]")
         else:
                 print("La opcion seleccionada no existe")
  
