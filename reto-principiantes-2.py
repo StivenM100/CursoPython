425577201f29d22f70622692e9cd35b6a89f6d51
StivenM100 <stiventingo20@gmail.com>
2024-03-26 01:10:19 -0500

    Tarea 2

--- reto-principiantes-2.py
+++ reto-principiantes-2.py
@@ -196,6 +196,192 @@ while bandera:
         nuevo = salario + aumento
 
         print(f'Novo salario: {nuevo:.2f}\nReajuste ganho: {aumento:.2f}\nEm percentual: {porcentaje} %')
+    elif opcion == 1049:
+        # Ejercicio 1049
+        a = input("Ingrese clase 1 de animal en base a la tabla: \n")
+        b = input("Ingrese clase 2 de animal en base a la tabla: \n")
+        c = input("Ingrese clase 3 de animal en base a la tabla: \n")
+
+        if a == 'vertebrado':
+            if b == 'ave':
+                if c == 'carnivoro':
+                    print("aguia")
+                else:
+                    print("pomba")
+            else:
+                if c == 'onivoro':
+                    print("homem")
+                else:
+                    print("vaca")
+        else:
+            if b == 'inseto':
+                if c == 'hematofago':
+                    print("pulga")
+                else:
+                    print("lagarta")
+            else:
+                if c == 'hematofago':
+                    print("sanguessuga")
+                else:
+                    print("minhoca")
+    elif opcion == 1050:
+        # Ejercicio 1050
+        codigo = int(input("Ingrese codigo de área para marcación telefónica: \n"))
+        diccionario = {61:'Brasilia',71:'Salvador',11:'Sao Paulo',21:'Rio de Janeiro',32:'Juiz de Fora',19:'Campinas',27:'Vitoria',31:'Belo Horizonte'}
+
+        if codigo in diccionario:
+            print(diccionario[codigo])
+        else:
+            print("DDD nao cadastrado")
+    elif opcion == 1051:
+        # Ejercicio 1051
+        salario = float(input("Ingrese salario: \n")) - 2000
+
+        if salario <= 0:
+            print("Isento")
+        elif salario <= 1000:
+            impuesto = salario * 0.08
+            print(f"R$ {impuesto:.2f}")
+        elif salario <= 2500:
+            impuesto = 1000 * 0.08 + (salario - 1000) * 0.18
+            print(f"R$ {impuesto:.2f}")
+        else:
+            impuesto = 1000 * 0.08 + 1500 * 0.18 + (salario - 2500) * 0.28
+            print(f"R$ {impuesto:.2f}")
+    elif opcion == 1052:
+        # Ejercicio 1052
+        numero = int(input("Ingrese número del mes a imprimir: \n"))
+        diccionario = {1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
+        print(diccionario[numero])
+    elif opcion == 1059:
+        # Ejercicio 1059
+        print("Los números pares entre 1 y 100 son:")
+        for numero in range(2,101,2):
+            print(numero)
+    elif opcion == 1060:
+        # Ejercicio 1060
+        contador = 0
+        for i in range(6):
+            numeros = float(input("Ingrese número: \n"))
+            if numeros > 0:
+                contador += 1
+        print(f"{contador} valores positivos")
+    elif opcion == 1061:
+        # Ejercicio 1061
+        dia_1 = int(input("Ingrese dia del inicio del evento: (Escribir 'Dia' antes del numero separado por espacio : [Dia n]) \n").split()[1])
+        tiempo_1 = input("Ingrese hora,minuto,segundo del inicio del evento [h : m : s] \n").split(" : ")
+        h1 = int(tiempo_1[0])
+        m1 = int(tiempo_1[1])
+        s1 = int(tiempo_1[2])
+
+        dia_2 = int(input("Ingrese dia del fin del evento: (Escribir 'Dia' antes del numero separado por espacio : [Dia n]) \n").split()[1])
+        tiempo_2 = input("Ingrese hora,minuto,segundo del fin del evento [h : m : s] \n").split(" : ")
+        h2 = int(tiempo_2[0])
+        m2 = int(tiempo_2[1])
+        s2 = int(tiempo_2[2])
+
+        d = dia_2 - dia_1
+        h = h2 - h1
+        m = m2 - m1
+        s = s2 - s1
+
+        if s < 0:
+            s = s + 60
+            m = m -1
+        if m < 0:
+            m = m + 60
+            h = h -1
+        if h < 0:
+            h = h + 24
+            d = d - 1
+        print(f"{d} dia(s)\n{h} hora(s)\n{m} minuto(s)\n{s} segundo(s)")
+    elif opcion == 1064:
+        # Ejercicio 1064
+        contador = 0
+        suma = 0
+        for i in range(6):
+            numeros = float(input("Ingrese número: \n"))
+            if numeros > 0:
+                contador += 1
+                suma += numeros
+        promedio = suma / contador
+        print(f"{contador} valores positivos\n{promedio:.1f}")
+    elif opcion == 1065:
+        # Ejercicio 1065
+        contador = 0
+        for i in range(5):
+            numeros = int(input("Ingrese número: \n"))
+            if numeros % 2 == 0:
+                contador += 1
+
+        print(f"{contador} valores pares")
+    elif opcion == 1066:
+        # Ejercicio 1066
+        contador_1 = 0
+        contador_2 = 0
+        contador_3 = 0
+        contador_4 = 0
+        for i in range(5):
+            numeros = int(input("Ingrese número: \n"))
+            if numeros % 2 == 0:
+                contador_1 += 1
+            else:
+                contador_2 += 1
+            if numeros > 0:
+                contador_3 += 1
+            elif numeros < 0:
+                contador_4 += 1
+
+        print(f"{contador_1} valor(es) par(es)")
+        print(f"{contador_2} valor(es) impar(es)")
+        print(f"{contador_3} valor(es) positivo(s)")
+        print(f"{contador_4} valor(es) negativo(s)")
+    elif opcion == 1067:
+        # Ejercicio 1067
+        valor = int(input("Ingrese el limite hasta donde quiere que se impirma los numeros impares del 1 a X\n"))
+        for numero in range(1,valor+1,2):
+            print(numero)
+    elif opcion == 1070:
+        # Ejercicio 1070
+        valor = int(input("Ingrese número desde donde que empiece a imprimir los números impares: \n"))
+        lista = []
+
+        while True:
+            if valor % 2 == 1:
+                print(valor)
+                lista.append(valor)
+                if len(lista) == 6:
+                    break
+            valor += 1
+    elif opcion == 1071:
+        # Ejercicio 1071
+        X = int(input("Ingrese el valor mayor desde donde inicie la sumatoria: \n"))
+        Y = int(input("Ingrese el valor menor hasta donde termine la sumatoria: \n"))
+        suma = 0
+        for i in range(X-1,Y,-1):
+            if i % 2 == 1:
+                suma += i
+                
+        print(suma)
+    elif opcion == 1072:
+        # Ejercicio 1072
+        dentro = 0
+        fuera = 0
+        for i in range(int(input("Ingrese el número de valores que serán leídos: \n"))):
+            numero = int(input("Ingrese número \n"))
+            if numero >= 10 and numero <= 20:
+                dentro += 1
+            else:
+                fuera += 1
+
+        print(f"{dentro} in")
+        print(f"{fuera} out")
+    elif opcion == 1073:
+        # Ejercicio 1073
+        numero = int(input("Ingrese el valor hasta donde quiere calcular el cuadrado de los números pares: \n"))
+        for i in range(1,numero+1):
+            if i % 2 == 0:
+                print(f"{i}^2 = {i**2}")
     else:
         print("La opcion seleccionada no existe")
     
